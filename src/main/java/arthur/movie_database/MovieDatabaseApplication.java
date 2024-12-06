3b8e72aaffefdc1e8422941b7e3d8aa96cf5595e
Arthur <112085855+arthurm0317@users.noreply.github.com>
2024-12-06 08:59:59 -0300

    first commit

---
+++ src/main/java/arthur/movie_database/MovieDatabaseApplication.java
@@ -0,0 +1,13 @@
+package arthur.movie_database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arthur/movie_database/MovieDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package arthur.movie_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
